d473ca8231e5fc7a3141b7fce8c651f808fdb1a8
stephen <sjarcher@ucsc.edu>
2026-04-02 11:35:29 -0700

    Begin adding platforming logic when scaffolding

--- v1_21_R1/src/main/java/me/stephenminer/v1_21_R1/pathfinder/BuilderPathfinder.java
+++ v1_21_R1/src/main/java/me/stephenminer/v1_21_R1/pathfinder/BuilderPathfinder.java
@@ -8,9 +8,13 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LadderBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class BuilderPathfinder extends InvasionPathfinder{
+
 
     public BuilderPathfinder(Level world, int fallDist, Set<Block> blacklist){
         super(world, fallDist, blacklist);
@@ -39,6 +43,7 @@ public class BuilderPathfinder extends InvasionPathfinder{
 
         Node tower = current;
         //complicated looking code final boss
+        // -1 is so that the place at the entities foot position will also have a pillar + ladder if they are just starting the scaffold. Otherwise we don't do this
         for (int i = segmentsToBuild != 4 ? 0 : -1; i < segmentsToBuild; i++){
             BlockPos pillar = pos.relative(dir).relative(Direction.UP, i);
             BlockState ladderState = Blocks.LADDER.defaultBlockState();
@@ -52,6 +57,90 @@ public class BuilderPathfinder extends InvasionPathfinder{
         return tower;
     }
 
+
+    /**
+     * Determines where platforms should be formed on a given path based on calculated scaffold heights
+     * @param path
+     * @return
+     */
+    private Set<Node> determinePlatforms(List<Node> path){
+        if (path.isEmpty()) return Set.of();
+        Set<Node> platformNodes = new HashSet<>();
+        int scaffoldHeight = 0;
+        Node prev = path.get(0);
+        for (int i = 1; i < path.size(); i++){
+            Node node = path.get(i);
+            if (nodeDyOnly(node, prev) && scaffoldNode(node) && scaffoldNode(prev)) {
+                if (scaffoldHeight % 4 == 0)
+                    platformNodes.add(node);
+                scaffoldHeight++;
+            }else scaffoldHeight = 0;
+        }
+        return platformNodes;
+    }
+
+    private BlockPos[] platformPositions(BlockPos pos){
+        return new BlockPos[]{
+                // Cardinal direction positions
+            pos.north(),
+                pos.east(),
+                pos.south(),
+                pos.west(),
+
+                // Diagonal positions (corners)
+                pos.north().east(),
+                pos.south().east(),
+                pos.south().west(),
+                pos.north().west()
+        };
+    }
+
+    private void injectPlatforms(Node node){
+        BlockPos[] positions = platformPositions(node.pos);
+        List<Node> additions = new ArrayList<>();
+        for (BlockPos pos : positions){
+            // Skip block pos if it is already in build target array
+            boolean skip = false;
+            if (node.buildTargets != null){
+                for (BlockPos taken : node.buildTargets){
+                    if (pos.equals(taken)){
+                        skip = true;
+                        break;
+                    }
+                }
+            }
+            if (skip) continue;
+            // Skip Block pos if the pos already contains a ladder or a solid block
+            BlockState state = world.getBlockState(pos);
+            if (isSolid(pos, state) && !isLadder(state)) continue;
+            additions.add(node);
+        }
+        int additionSize = additions.size();
+        int sizes = additionSize + (node.buildTargets == null ? 0 : node.buildTargets.length);
+        BlockPos[] targets = new BlockPos[sizes];
+        BlockState[] states = new BlockState[sizes];
+        final BlockState oakState = Blocks.OAK_PLANKS.defaultBlockState();
+
+        // add additional build targets first, we probably want the platform constructed before anything else...
+        int i;
+        for (i = 0; i < additions.size(); i++){
+            targets[i] = additions.get(i).pos;
+            states[i] = oakState;
+        }
+
+        // Restore original build decisions to end of array
+        for ( ; i < targets.length; i++){
+            targets[i] = node.buildTargets[i - additionSize];
+            states[i] = node.buildMats[i - additionSize];
+        }
+
+        node.buildTargets = targets;
+        node.buildMats = states;
+    }
+
+    private boolean nodeDyOnly(Node node1, Node node2){
+        return node2.y - node1.y > 0 && node2.x == node1.x && node2.z == node1.z;
+    }
 
     private boolean scaffoldNode(Node node){
         if (node.digTargets == null || node.buildMats== null) return false;
